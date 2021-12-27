e7208d44c511a7458e04e0730a113a029ddc49d7
Nuno Pereira Sousa <9180579@esmad.ipp.pt>
2021-12-27 20:58:44 +0000

    Infinite marquee working

--- src/components/headers/header.js
+++ src/components/headers/header.js
@@ -1,5 +1,48 @@
+import gsap from "gsap";
+import { useRef, useEffect } from "react";
+
 const Header = ({ title }) => {
-  return <h1 className="hero__title">{title}</h1>;
+  const h = useRef(null);
+  const spans = useRef([]);
+  // const spans = "";
+  const arr = [1, 2, 3, 4];
+
+  // if (typeof window !== "undefined") {
+  //   spans = document.querySelectorAll(".hero__title span");
+  //   console.log(spans);
+  // }
+
+  const marquee = _ => {
+    gsap
+      .to(spans.current, {
+        xPercent: -100,
+        repeat: -1,
+        duration: 8,
+        ease: "linear"
+      })
+      .totalProgress(0.5);
+
+    gsap.set(h.current, { xPercent: 0 });
+  };
+
+  useEffect(() => {
+    marquee();
+  }, []);
+
+  return (
+    <h1 className="hero__title" ref={h}>
+      {arr.map((span, index) => (
+        <span
+          key={span}
+          ref={element => {
+            spans.current[index] = element;
+          }}
+        >
+          {title}&nbsp;&nbsp;
+        </span>
+      ))}
+    </h1>
+  );
 };
 
 export default Header;
