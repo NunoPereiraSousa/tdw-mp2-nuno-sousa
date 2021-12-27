2c07f93b6253f4cd821c0bcde85ee55ccfe2565d
Nuno Pereira Sousa <9180579@esmad.ipp.pt>
2021-12-27 15:26:23 +0000

    Skeleton loading working for Home and Films page

--- src/pages/films.js
+++ src/pages/films.js
@@ -3,6 +3,7 @@ import Header from "../components/headers/header";
 import FilmsCard from "../components/films/filmCard";
 import Subheader from "../components/headers/subheader";
 import Footer from "../components/footer/footer";
+import SkeletonLoading from "../components/loading/SkeletonLoading";
 
 import { useEffect } from "react";
 import { useDispatch, useSelector, shallowEqual } from "react-redux";
@@ -16,7 +17,24 @@ const Films = _ => {
     dispatch(getAllFilms());
   }, []);
 
-  if (films.loading === "rejected") return "Error!";
+  const html = _ => {
+    if (films.loading === "rejected") return "Error!";
+    if (films.loading === "pending") {
+      return [1, 2, 3, 4, 5, 6].map(item => <SkeletonLoading key={item} />);
+    }
+    if (films.loading === "fulfilled") {
+      return films.films.map(film => (
+        <FilmsCard
+          key={film.episode_id}
+          id={film.episode_id}
+          title={film.title}
+          date={film.release_date}
+          director={film.director}
+          producer={film.producer}
+        />
+      ));
+    }
+  };
 
   return (
     <>
@@ -27,18 +45,7 @@ const Films = _ => {
       </section>
 
       <section className="home__films">
-        <div className="home__films__grid">
-          {films.films.map(film => (
-            <FilmsCard
-              key={film.episode_id}
-              id={film.episode_id}
-              title={film.title}
-              date={film.release_date}
-              director={film.director}
-              producer={film.producer}
-            />
-          ))}
-        </div>
+        <div className="home__films__grid">{html()}</div>
       </section>
 
       <Footer />
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import FilmsCard from "../components/films/filmCard";
 import Footer from "../components/footer/footer";
 import Header from "../components/headers/header";
 import Subheader from "../components/headers/subheader";
+import SkeletonLoading from "../components/loading/SkeletonLoading";
 // import Component from "../classes/Component";
 import { useEffect } from "react";
 // import gsap from "gsap";
@@ -57,8 +58,24 @@ export default function Home() {
     dispatch(getAllFilms());
   }, []);
 
-  // if (loading === "pending" || loading === "idle") return "Loading...";
-  if (films.loading === "rejected") return "Error!";
+  const html = _ => {
+    if (films.loading === "rejected") return "Error!";
+    if (films.loading === "pending") {
+      return [1, 2, 3, 4, 5, 6].map(item => <SkeletonLoading key={item} />);
+    }
+    if (films.loading === "fulfilled") {
+      return films.films.map(film => (
+        <FilmsCard
+          key={film.episode_id}
+          id={film.episode_id}
+          title={film.title}
+          date={film.release_date}
+          director={film.director}
+          producer={film.producer}
+        />
+      ));
+    }
+  };
 
   return (
     <>
@@ -103,17 +120,7 @@ export default function Home() {
       </section>
 
       <section className="home__films">
-        <div className="home__films__grid">
-          {films.films.map(film => (
-            <FilmsCard
-              key={film.episode_id}
-              title={film.title}
-              date={film.release_date}
-              director={film.director}
-              producer={film.producer}
-            />
-          ))}
-        </div>
+        <div className="home__films__grid">{html()}</div>
       </section>
 
       <Footer />
